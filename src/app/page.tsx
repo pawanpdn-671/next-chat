cad939724cd8276e60822b8a60d5340702f6edc5
pawanpdn-671 <pawanpradhanpc671@gmail.com>
2024-01-26 23:30:02 +0530

    update:next-auth init done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "./page.module.css";
+import Button from "@/components/ui/Button";
+import { db } from "@/lib/db";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-	return <div className="text-red-500">hello world</div>;
+export default async function Home() {
+	await db.set("hello", "hello");
+	return <Button variant={"default"}>Hello</Button>;
 }
